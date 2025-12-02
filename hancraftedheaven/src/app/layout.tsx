dffbe5bf8b7739ee1fa3382c1ab650aacaa60d74
Cherry-Machado <cherry5467@gmail.com>
2025-12-02 03:06:46 -0300

    updating

--- hancraftedheaven/src/app/layout.tsx
+++ hancraftedheaven/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+/* import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./ui/styles/globals.css";
 import Footer from "./ui/footer";
@@ -18,10 +18,38 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 text-gray-900`}>
-        {/* <Header /> */}
-        <main className="min-h-screen">{children}</main>
+        {/* <Header /> */
+       /*  <main className="min-h-screen">{children}</main>
         <Footer />
       </body>
+    </html>
+  );
+} */
+ 
+import { Inter } from "next/font/google";
+import '@/app/ui/global.css';
+/* import { inter } from '@/app/ui/fonts';  */
+import { Metadata } from 'next';
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+   title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      {/* <body suppressHydrationWarning={true}>{children}</body> */}
+      {<body className={`${inter.className}  antialiased`} suppressHydrationWarning={true}>{children}</body>}
     </html>
   );
 }
